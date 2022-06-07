781fcb59415cb097039fb599d446e8f2d880427e
Mason Hamilton <Hamim949@hsd.k12.or.us>
2022-06-07 08:56:28 -0700

    started working on images

---
+++ scripts.js
@@ -0,0 +1,39 @@
+var mobileBreakpoint = 600;
+
+window.onload = function() {
+    var screenWidth = window.innerWidth;
+    var globalNavbar = document.getelementById("global-nav");
+    var toggle = document.getElementById("menu-toggle");
+}
+if (screenWidth > mobileBreakpoint) {
+    globalNavbar.classlist.add("open");
+    toggle.classList.add("open");
+}
+
+//Add a toggle click function
+toggle.onclick = function() {
+    if (globalNavbar.classlist.contains("open")) {
+        globalNavbar.classlist.remove("open");
+        toggle.classList.remove("open");
+    }
+    else {
+        globalNavbar.classlist.add("open");
+        toggle.classList.add("add");
+    }
+}
+
+window.onresize = function() {
+    var screenWWidth = window.innerWidth;
+
+    var globalNavbar = document.getElementById("global-nav");
+    var toggle = document.getElementById("menu-toggle");
+
+    if (screenwidth > mobileBreakpoint) {
+        globalNavbar.classList.add("open");
+        toggle.classList.add("open");
+    }
+    else {
+        globalNavbar.classList.remove("open");
+        toggle.classList.remove("open");
+    }
+}
